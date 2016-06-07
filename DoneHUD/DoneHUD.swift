3a6cbb9ab1a55e0a1bf4dea5c143bf52e08f76da
Paweł Bednorz <pawel@quver.pl>
2016-06-07 23:02:06 +0200

    Refactor

--- DoneHUD/DoneHUD.swift
+++ DoneHUD/DoneHUD.swift
@@ -8,23 +8,26 @@
 
 import UIKit
 
-public class DoneHUD: NSObject {
+public class DoneHUD {
+
     private static let sharedObject = DoneHUD()
     let doneView = DoneView()
 
-    public static func showInView(view: UIView) {
+    public class func showInView(view: UIView) {
         DoneHUD.sharedObject.showInView(view, message: nil)
     }
     
-    public static func showInView(view: UIView, message: String?) {
+    public class func showInView(view: UIView, message: String?) {
         DoneHUD.sharedObject.showInView(view, message: message)
     }
     
     private func showInView(view: UIView, message: String?) {
         // Set size of done view
         let doneViewWidth = min(view.frame.width, view.frame.height) / 2
-        var originX: CGFloat, originY: CGFloat
-        if (UIDevice.currentDevice().systemVersion as NSString).floatValue >= 8.0 {
+        var originX: CGFloat
+        var originY: CGFloat
+      
+        if #available(iOS 8.0, *) {
             originX = (view.frame.width - doneViewWidth) / 2
             originY = (view.frame.height - doneViewWidth) / 2
         } else {
@@ -32,31 +35,29 @@ public class DoneHUD: NSObject {
             originX = ((isLandscape ? view.frame.height : view.frame.width) - doneViewWidth) / 2
             originY = ((isLandscape ? view.frame.width : view.frame.height) - doneViewWidth) / 2
         }
-        let doneViewFrame = CGRectMake(
-            originX,
-            originY,
-            doneViewWidth,
-            doneViewWidth)
-        self.doneView.layer.cornerRadius = 8
-        self.doneView.frame = doneViewFrame
+
+        let doneViewFrame = CGRect(x: originX, y: originY, width: doneViewWidth, height: doneViewWidth)
+        doneView.layer.cornerRadius = 8
+        doneView.frame = doneViewFrame
 
         // Set message
-        self.doneView.setMessage(message)
+        doneView.message = message
         
         // Start animation
-        self.doneView.alpha = 0
-        view.addSubview(self.doneView)
-        UIView.animateWithDuration(0.2, animations: { () -> Void in
-            self.doneView.alpha = 1
-        }) { (result: Bool) -> Void in
-            self.doneView.drawCheck({ () -> Void in
+        doneView.alpha = 0
+        view.addSubview(doneView)
+
+        UIView.animateWithDuration(0.2, animations: { [weak self] in
+            self?.doneView.alpha = 1
+        }) { [weak self] (result: Bool) -> Void in
+            self?.doneView.drawCheck({ [weak self] in
                 let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
                 dispatch_after(delayTime, dispatch_get_main_queue()) {
-                    UIView.animateWithDuration(0.2, animations: { () -> Void in
-                        self.doneView.alpha = 0
-                        }) { (result: Bool) -> Void in
-                            self.doneView.removeFromSuperview()
-                            self.doneView.clear()
+                    UIView.animateWithDuration(0.2, animations: { [weak self] in
+                        self?.doneView.alpha = 0
+                        }) { [weak self] (result: Bool) -> Void in
+                            self?.doneView.removeFromSuperview()
+                            self?.doneView.clear()
                     }
                 }
             })
--- DoneHUD/DoneView.swift
+++ DoneHUD/DoneView.swift
@@ -10,103 +10,92 @@ import UIKit
 
 public class DoneView: UIView {
 
-    // MARK: - private variables
-    
+    public var message: String? {
+        didSet {
+            if let messageLabel = messageLabel {
+                messageLabel.text = message
+                messageLabel.frame = CGRect(
+                    x: 8,
+                    y: frame.height / 5 * 4 - 10,
+                    width: frame.width - 16,
+                    height: 20)
+
+            } else {
+                messageLabel = UILabel()
+                messageLabel!.numberOfLines = 1
+                messageLabel!.textAlignment = .Center
+                messageLabel!.lineBreakMode = .ByTruncatingTail
+                messageLabel!.textColor = UIColor.blackColor()
+                addSubview(messageLabel!)
+            }
+        }
+    }
+
     private let lineLayer: CAShapeLayer = CAShapeLayer()
-    private var message: String? = nil
-    private var messageLabel: UILabel? = nil
-    private var blurView: UIView? = nil
-    
-    // MARK: - public methods
+    private var messageLabel: UILabel?
+    private var blurView: UIView?
     
     public init() {
         super.init(frame: CGRectZero)
-        self.initialize()
+        initialize()
     }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        self.initialize()
+        initialize()
     }
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.initialize()
-    }
-    
-    public func setMessage(message: String?) {
-        self.message = message
-        if let message = self.message {
-            if self.messageLabel == nil {
-                let messageLabel = UILabel()
-                self.messageLabel = messageLabel
-                messageLabel.numberOfLines = 1
-                messageLabel.textAlignment = .Center
-                messageLabel.lineBreakMode = .ByTruncatingTail
-                messageLabel.textColor = UIColor.blackColor()
-                self.addSubview(messageLabel)
-            }
-            if let messageLabel = self.messageLabel {
-                messageLabel.text = message
-                messageLabel.frame = CGRectMake(
-                    8,
-                    self.frame.height / 5 * 4 - 10,
-                    self.frame.width - 16,
-                    20)
-            }
-        }
+        initialize()
     }
     
     public func drawCheck(completion: (() -> Void)?) {
-        let canvasFrame = CGRectMake(
-            self.frame.width / 4,
-            message == nil ? self.frame.height / 3 : self.frame.height / 5 * 2,
-            self.frame.width / 2,
-            self.frame.height / 3)
+        let canvasFrame = CGRect(
+          x: frame.width / 4,
+          y: message == nil ? frame.height / 3 : frame.height / 5 * 2,
+          width: frame.width / 2,
+          height: frame.height / 3)
+
         let path = UIBezierPath()
-        path.moveToPoint(
-            CGPointMake(canvasFrame.origin.x, canvasFrame.origin.y + canvasFrame.height / 2))
-        path.addLineToPoint(
-            CGPointMake(canvasFrame.origin.x + canvasFrame.width / 3, canvasFrame.origin.y))
-        path.addLineToPoint(
-            CGPointMake(canvasFrame.origin.x + canvasFrame.width, canvasFrame.origin.y + canvasFrame.height))
-        self.lineLayer.frame = self.bounds
-        self.lineLayer.geometryFlipped = true
-        self.lineLayer.path = path.CGPath
+        path.moveToPoint(CGPointMake(canvasFrame.origin.x, canvasFrame.origin.y + canvasFrame.height / 2))
+        path.addLineToPoint(CGPointMake(canvasFrame.origin.x + canvasFrame.width / 3, canvasFrame.origin.y))
+        path.addLineToPoint(CGPointMake(canvasFrame.origin.x + canvasFrame.width, canvasFrame.origin.y + canvasFrame.height))
+        lineLayer.frame = bounds
+        lineLayer.geometryFlipped = true
+        lineLayer.path = path.CGPath
         
-        self.layer.addSublayer(self.lineLayer)
-        self.animate(completion)
+        layer.addSublayer(lineLayer)
+        animate(completion)
     }
     
     public func clear() {
-        self.lineLayer.removeFromSuperlayer()
-        self.lineLayer.removeAllAnimations()
-        self.lineLayer.path = nil
+        lineLayer.removeFromSuperlayer()
+        lineLayer.removeAllAnimations()
+        lineLayer.path = nil
 
-        self.messageLabel?.removeFromSuperview()
-        self.messageLabel = nil
-        self.message = nil
+        messageLabel?.removeFromSuperview()
+        messageLabel = nil
+        message = nil
     }
     
     override public func layoutSubviews() {
         super.layoutSubviews()
-        self.blurView?.frame = self.bounds
+        blurView?.frame = bounds
     }
-    
-    // MARK: - private methods
-    
+
     private func initialize() {
         // Initialize properties
-        self.clipsToBounds = true
+        clipsToBounds = true
         
         // Set default setting to line
-        self.lineLayer.fillColor = UIColor.clearColor().CGColor
-        self.lineLayer.anchorPoint = CGPointMake(0, 0)
-        self.lineLayer.lineJoin = kCALineJoinRound
-        self.lineLayer.lineCap = kCALineCapRound
-        self.lineLayer.contentsScale = self.layer.contentsScale
-        self.lineLayer.lineWidth = 8
-        self.lineLayer.strokeColor = UIColor.blackColor().CGColor
+        lineLayer.fillColor = UIColor.clearColor().CGColor
+        lineLayer.anchorPoint = CGPointMake(0, 0)
+        lineLayer.lineJoin = kCALineJoinRound
+        lineLayer.lineCap = kCALineCapRound
+        lineLayer.contentsScale = layer.contentsScale
+        lineLayer.lineWidth = 8
+        lineLayer.strokeColor = UIColor.blackColor().CGColor
         
         // Generate blur view
         var blurView: UIView
@@ -116,9 +105,11 @@ public class DoneView: UIView {
             blurView = UIView()
             blurView.backgroundColor = UIColor(white: 1, alpha: 0.8)
         }
+
         self.blurView = blurView
-        self.insertSubview(blurView, atIndex: 0)
-        blurView.frame = self.bounds
+
+        insertSubview(blurView, atIndex: 0)
+        blurView.frame = bounds
     }
     
     private func animate(completion: (() -> Void)?) {
@@ -130,7 +121,7 @@ public class DoneView: UIView {
         if let completion = completion {
             CATransaction.setCompletionBlock(completion)
         }
-        self.lineLayer.addAnimation(pathAnimation, forKey:"strokeEndAnimation")
+        lineLayer.addAnimation(pathAnimation, forKey:"strokeEndAnimation")
         CATransaction.commit()
     }
 }
